640ff5667c2531c69367a60aefcfb61271a905fc
yyj9529 <garethgates7@naver.com>
2025-06-08 23:18:24 +0900

    leaderboard complete

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,18 +1,37 @@
 import type { Route } from "../../../../.react-router/types/app/features/products/pages/+types/daily-leaderboard-page";
 import { DateTime } from "luxon";
 import { data, isRouteErrorResponse } from "react-router";
+import {z} from "zod";
+import LeaderboardPage from "./leaderboard-page";
+import { LeaderboardHeader } from "~/common/components/ui/leaderboard-header";
+import { ProductCard } from "../components/product-card";
+
+
+const paramsSchema = z.object({
+  year : z.coerce.number(),
+  month : z.coerce.number(),
+  day : z.coerce.number(),
+});
+
 
 export const meta: Route.MetaFunction = () => [
   { title: "Daily Leaderboard Page" },
 ];
 
 export const loader =({params}:Route.LoaderArgs)=>{
-    const {year,month,day} = params;
-    const date = DateTime.fromObject({
-      year : Number(year),
-      month : Number(month),
-      day : Number(day),
-    }).setZone("Asia/Seoul");
+    const {success,data:parsedData} = paramsSchema.safeParse(params);
+
+    if(!success){
+      throw data({
+        error_code : "invalid_params",
+        message : "Invalid params",
+      },{
+        status : 400
+      });
+    }
+
+
+    const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
 
     if(!date.isValid){
       throw data({
@@ -23,14 +42,51 @@ export const loader =({params}:Route.LoaderArgs)=>{
       }
     );
     }
+    const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+  
+    if(date > today){
+      throw data(
+        {
+          error_code : "future_date",
+          message : "Future date",
+        },
+        {
+          status : 400
+        }
+      );
+    }
+    return {
+      ...parsedData
+    };
 }
 
 export function action({ request }: Route.ActionArgs) {
   return {};
 }
 
-export default function DailyLeaderboardPage(props: Route.ComponentProps) {
-  return <div>Daily Leaderboard Page</div>;
+export default function DailyLeaderboardPage({loaderData}: Route.ComponentProps) {
+//   const date = DateTime.fromObject(loaderData).setZone("Asia/Seoul");
+   const date = DateTime.fromObject(loaderData).setZone("Asia/Seoul");
+    return(
+      <div>
+        <LeaderboardHeader
+        title={date.toLocaleString(DateTime.DATE_MED)}
+        description="The most popular products on maksers"
+      />
+      <div className="space-y-5 w-full max-x-screen-md mx-auto">
+        {Array.from({length:10}).map((_,index)=>(  
+            <ProductCard 
+              key={index} 
+              productId={`productId-${index}`} 
+              productName="Sample Product Name" 
+              productDescription="This is a sample product description." 
+              commentCount={12} 
+              upvoteCount={120}
+              />
+          ))}
+      </div>
+      </div>
+    )
 } 
 
 export function ErrorBoundary({error}:Route.ErrorBoundaryProps){
